ec37ed19579f772d7c94aa6f01c31177b650e6d2
Dave Hards <unstablesun@gmail.com>
2017-10-01 20:27:59 -0700

    formating python

--- WorldMap.py
+++ WorldMap.py
@@ -1,43 +1,60 @@
 
 
+import random
 
-Location0 = {'id': 0, 'north': 'area1', 'west': 'blocked', 'south': 'blocked', 'east': 'blocked', 'description': 'you are in a clearing'}
-Location1 = {'id': 1, 'north': 'area2', 'west': 'area3', 'south': 'blocked', 'east': 'blocked', 'description': 'you are in a wooded area with spoungy turf'}
-Location2 = {'id': 2, 'north': 'area3', 'west': 'blocked', 'south': 'blocked', 'east': 'blocked', 'description': 'you are at the edge of a cliff over looking a river'}
-Location3 = {'id': 3, 'north': 'blocked', 'west': 'blocked', 'south': 'blocked', 'east': 'area1', 'description': 'you are at mouth of a cave blocked by dead bodies'}
 
-WorldLocations = {'area0':Location0,'area1':Location1,'area2':Location2,'area3':Location3}
+Danger0 = {'description': 'You see a swarm of red and yellow wasps!',
+           'power': 2,
+           'attack': 'you are in a clearing',
+           'defend': 'you duck and run close to the ground',
+           'damage': 'you get stung several times in the face'}
+
+
+Location0 = {'id': 0, 'north': 'area1', 'west': 'blocked', 'south': 'blocked', 'east': 'blocked',
+             'description': 'you are in a clearing'}
+Location1 = {'id': 1, 'north': 'area2', 'west': 'area3', 'south': 'blocked', 'east': 'blocked',
+             'description': 'you are in a wooded area with spoungy turf'}
+Location2 = {'id': 2, 'north': 'area3', 'west': 'blocked', 'south': 'blocked', 'east': 'blocked',
+             'description': 'you are at the edge of a cliff over looking a river'}
+Location3 = {'id': 3, 'north': 'blocked', 'west': 'blocked', 'south': 'blocked', 'east': 'area1',
+             'description': 'you are at mouth of a cave blocked by dead bodies'}
+
+WorldLocations = {'area0': Location0, 'area1': Location1, 'area2': Location2, 'area3': Location3}
 
 
 CurrentLocation = 'area0'
 ActionResponseText = 'none'
+PlayerHealth = 'good'
 
 
-def MoveFromLocation(direction):
+def move_from_location(direction):
     global ActionResponseText
     global CurrentLocation
-    curloc = WorldLocations[CurrentLocation]
-    direction_value = curloc[direction]
+    location = WorldLocations[CurrentLocation]
+    direction_value = location[direction]
+    # chance = random.randint(0, 100)
     if direction_value == 'blocked':
         ActionResponseText = "The way is blocked"
         print(direction_value + ActionResponseText)
     else:
         print("Moving to " + direction_value + " yes the way is clear")
         CurrentLocation = direction_value
-        curloc = WorldLocations[CurrentLocation]
-        ActionResponseText = curloc['description']
+        current_location = WorldLocations[CurrentLocation]
+        ActionResponseText = current_location['description']
         print("new location desc " + ActionResponseText)
 
-def WaitAtLocation():
+
+def wait_at_location():
     global ActionResponseText
     ActionResponseText = "You better do something"
 
-def DescribeLocation():
-    global ActionResponseText
-    curloc = WorldLocations[CurrentLocation]
-    ActionResponseText = curloc['description']
 
-#commands
+def describe_location():
+    global ActionResponseText
+    current_location = WorldLocations[CurrentLocation]
+    ActionResponseText = current_location['description']
+
+
 '''
 describe
 move north
@@ -49,44 +66,42 @@ defend
 hide
 wait'''
 
-def ParseCommand(command):
+
+def parse_command(command):
     if command == 'move north':
-        MoveFromLocation('north')
-    elif command== 'move west':
-        MoveFromLocation('west')
-    elif command== 'move south':
-        MoveFromLocation('south')
-    elif command== 'move east':
-        MoveFromLocation('east')
-    elif command== 'describe':
-        DescribeLocation()
-    elif command== 'wait':
-        WaitAtLocation()
-    elif command== 'attack':
-        WaitAtLocation()
-    elif command== 'defend':
-        WaitAtLocation()
-    elif command== 'hide':
-        WaitAtLocation()
+        move_from_location('north')
+    elif command == 'move west':
+        move_from_location('west')
+    elif command == 'move south':
+        move_from_location('south')
+    elif command == 'move east':
+        move_from_location('east')
+    elif command == 'describe':
+        describe_location()
+    elif command == 'wait':
+        wait_at_location()
+    elif command == 'attack':
+        wait_at_location()
+    elif command == 'defend':
+        wait_at_location()
+    elif command == 'hide':
+        wait_at_location()
 
 
+# don't copy below this line
 
-
-
-#don't copy below this line
 
 def main():
     startloc = WorldLocations[CurrentLocation]
-
 
     for key in startloc:
         print(key)
         value = startloc[key]
         print(value)
 
-    ParseCommand('move north')
-    ParseCommand('move west')
-    ParseCommand('move east')
+    parse_command('move north')
+    parse_command('move west')
+    parse_command('move east')
 
 
 if __name__ == "__main__":
